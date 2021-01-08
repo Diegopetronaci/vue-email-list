d6b584cb0eb9a5ddedca74fc2d4a200c2f4e5bd1
Diego <Petronacidiego@gmail.com>
2021-01-08 15:27:57 +0100

    esercizio da controllare

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+let app = new Vue ({
+
+    el: '#app',
+    data: {
+        //emailCasuale: "",
+        emailCasuale: [],
+    },
+    methods: {
+
+    },
+    mounted(){
+        for (let i = 0; i < 9; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(resp => {
+                console.log(resp.data.response);
+                /* let email = resp.data.response;
+                this.emailCasuale = email; */
+                this.emailCasuale.push(resp.data.response);
+            })
+        }
+    }
+})
