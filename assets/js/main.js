17819f68149c450e3793bfdea6e2dcda428a8921
Diego <Petronacidiego@gmail.com>
2021-01-11 10:38:10 +0100

    corretto numero email ed incrementato di 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,9 @@ let app = new Vue ({
 
     },
     mounted(){
-        for (let i = 0; i < 9; i++) {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        for (let i = 0; i < 10; i++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(resp => {
                 console.log(resp.data.response);
                 /* let email = resp.data.response;
